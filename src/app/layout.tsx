d08925ca8c455c6d3042e8569e0fe5eac0b4232f
Adithya Mattappily <74791365+KarmaChild@users.noreply.github.com>
2024-05-26 23:45:14 -0600

    update name to Matchflix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Indian Harmonies",
-  description: "Indian Harmonies",
-};
+  title: "Matchflix",
+  description: "Movie Matching Game - Find four squares with a common theme. All are related to movies!",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
